e07b406c8eee28135d65219a8fd7ba2c131a9db0
AmanShahAhamed <moaslam826@gmail.com>
2022-06-17 12:30:25 +0800

    Adding another router user and controller profile

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ console.log('router is loaded');
 const homeController=require('../controllers/home_controller')
 
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
 
 
 
