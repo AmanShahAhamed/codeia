d6f72baaf9c872d75b8580cd57e2db19ff0c0078
AmanShahAhamed <moaslam826@gmail.com>
2022-06-17 12:34:00 +0800

    adding another route in user router named post and a controller in user controlller named posts

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -2,3 +2,6 @@ module.exports.profile=function(req,res){
     res.end('<h1>User Profile is Here..</h1>');
 }
 
+module.exports.posts=function(req,res){
+    res.end('<h1>User Post show here ...</h1>');
+}
