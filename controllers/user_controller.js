982f073d3d26db1d756cac30ba6da079067bb1f2
AmanShahAhamed <moaslam826@gmail.com>
2022-06-19 03:20:16 +0800

    signin page authefication is completed by using cookie

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -2,7 +2,22 @@
 const User=require('../models/user');
 
 module.exports.profile=function(req,res){
-    res.end('<h1>User Profile is Here..</h1>');
+    
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id,function(err,user){
+            if(err){console.log('There is problem in finding cookies user_id'); return}
+            if(user){
+                return res.render('profile',{
+                    tittle:'user_profile',
+                    user:user
+                })
+            }else{
+                return res.redirect('/users/sign-in');
+            }
+        });
+    }else{
+        return res.redirect('/users/sign-in');
+    }
 }
 
 module.exports.posts=function(req,res){
@@ -43,5 +58,22 @@ module.exports.create=function(req,res){
 
 //create user sign in session
 module.exports.createSession=function(req,res){
-    //TODO
+    
+    User.findOne({email:req.body.email},function(err,user){
+      
+        if(err){console.log('Error in Finding email..'); return}
+        if(user){
+            if(user.password!=req.body.password){
+                console.log('Something wrong with password please look at it..');
+                return res.redirect('back');
+            }
+            res.cookie('user_id',user.id);
+            return res.redirect('/users/profile');
+
+        }else{
+            console.log('Something wrong with email please look at it..');
+            return res.redirect('back');
+        }
+
+    });
 }
